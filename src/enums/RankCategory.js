48b833b9e24b25e87182957d2c9604c36a4c111f
marcusrprojects <marcusr4231@gmail.com>
2024-10-30 10:12:50 -0400

    Support for turning rank tier numbers into their category names.

--- src/enums/RankCategory.js
+++ src/enums/RankCategory.js
@@ -4,4 +4,12 @@ const RankCategory = Object.freeze({
   GOOD: 2,
 });
 
+// Function to map enum number to the category name
+export const getRankCategoryName = (rankValue) => {
+  return (
+    Object.keys(RankCategory).find((key) => RankCategory[key] === rankValue) ||
+    "Unknown"
+  );
+};
+
 export default RankCategory;
