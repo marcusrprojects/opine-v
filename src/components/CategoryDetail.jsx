3f0898ede2d1bd924bf55c787bafdf1db7e91077
marcusrprojects <marcusr4231@gmail.com>
2024-11-05 20:31:11 -0500

    onClick to pass on cardColor for proper itemview styling.

--- src/components/CategoryDetail.jsx
+++ src/components/CategoryDetail.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { db } from '../firebaseConfig';
 import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { FaPlusCircle } from 'react-icons/fa';
 import "../styles/CategoryDetail.css";
-import { debounce } from 'lodash'; // To debounce filtering
+import { debounce } from 'lodash';
 import RankCategory from '../enums/RankCategory';
 import LoadingMessages from '../enums/LoadingMessages';
 import LoadingComponent from './LoadingComponent';
@@ -29,7 +29,7 @@ const CategoryDetail = () => {
         if (categorySnapshot.exists()) {
           const categoryData = categorySnapshot.data();
           fields.current = categoryData.fields;
-          primaryField.current = categoryData.primaryField; // Fetch primaryField
+          primaryField.current = categoryData.primaryField;
           categoryName.current = categoryData.name;
         }
 
@@ -68,11 +68,15 @@ const CategoryDetail = () => {
       });
     });
     setFilteredItems(filtered);
-  }, 300); // Adjust debounce timing as necessary
+  }, 300);
 
   useEffect(() => {
     applyFilters();
   }, [filters, applyFilters]);
+
+  const handleItemClick = (itemId, cardColor) => {
+    navigate(`./item/${itemId}`, { state: { cardColor } });
+  };
 
   if (loading) {
     return <LoadingComponent message={LoadingMessages.FETCHING} />;
@@ -96,35 +100,37 @@ const CategoryDetail = () => {
         </div>
 
         <div className="item-grid">
-          {filteredItems.map((item, index) => {
+          {filteredItems.map((item) => {
             const rating = item.rating || 1;
-            const rankCategory = item.rankCategory ?? RankCategory.OKAY; // Default to 'Okay' category if undefined
+            const rankCategory = item.rankCategory ?? RankCategory.OKAY;
             
             const maxWhite = 50;
-            const hues = [0, 60, 120]; // HSL hues for Bad (0), Okay (60), Good (120)
-            const thresholds = [0, (1 / 3) * 10, (2 / 3) * 10]; // Rating thresholds for each category
+            const hues = [0, 60, 120];
+            const thresholds = [0, (1 / 3) * 10, (2 / 3) * 10];
             
-            const adjustedWhiteness = maxWhite - (rating - thresholds[rankCategory]) * (50/3);
+            const adjustedWhiteness = maxWhite - (rating - thresholds[rankCategory]) * (50 / 3);
             const cardColor = `hwb(${hues[rankCategory]} ${adjustedWhiteness}% 17.5%)`;
             const itemId = item.id;
 
             return (
-              <Link to={`./item/${item.id}`} key={itemId}>
-                <div key={index} className="item-card" style={{ borderColor: cardColor }}>
-                  <div className="item-header">
-                    <div className="item-rating" style={{ borderColor: cardColor }}>{rating.toFixed(1)}</div>
-                    {/* Use primaryField for the item title */}
-                    <h4 className="item-title">{item[primaryField.current] || "Unnamed Item"}</h4>
-                  </div>
-                  <div className="item-content" style={{ backgroundColor: cardColor }}>
-                    {fields.current.filter(field => field !== primaryField.current).map((field, fieldIndex) => (
-                      <p key={fieldIndex}>
-                        {field}: {item[field] || "N/A"}
-                      </p>
-                    ))}
-                  </div>
+              <div 
+                key={itemId} 
+                className="item-card" 
+                style={{ borderColor: cardColor }} 
+                onClick={() => handleItemClick(itemId, cardColor)}
+              >
+                <div className="item-header">
+                  <div className="item-rating" style={{ borderColor: cardColor }}>{rating.toFixed(1)}</div>
+                  <h4 className="item-title">{item[primaryField.current] || "Unnamed Item"}</h4>
                 </div>
-              </Link>
+                <div className="item-content" style={{ backgroundColor: cardColor }}>
+                  {fields.current.filter(field => field !== primaryField.current).map((field, fieldIndex) => (
+                    <p key={fieldIndex}>
+                      {field}: {item[field] || "N/A"}
+                    </p>
+                  ))}
+                </div>
+              </div>
             );
           })}
         </div>
