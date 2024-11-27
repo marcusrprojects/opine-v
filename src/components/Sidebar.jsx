ea95a08617e54d9f83e89b7322612ec86dffe550
marcusrprojects <marcusr4231@gmail.com>
2024-11-26 23:24:11 -0500

    useEffect s.t. sidebar automatically closes 4 seconds after opening.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Sidebar.css";
 import PropTypes from "prop-types";
@@ -5,6 +6,18 @@ import { FaBars, FaUser, FaHome, FaSignInAlt } from "react-icons/fa";
 import ThemeToggle from "./ThemeToggle";
 
 const Sidebar = ({ sidebarOpen, toggleSidebar }) => {
+  useEffect(() => {
+    let timeout;
+    if (sidebarOpen) {
+      timeout = setTimeout(() => {
+        toggleSidebar(); // Close the sidebar
+      }, 4000); // 4 seconds
+    }
+
+    // Cleanup the timeout if the component unmounts or sidebarOpen changes
+    return () => clearTimeout(timeout);
+  }, [sidebarOpen, toggleSidebar]);
+
   return (
     <>
       {/* Sidebar toggle button */}
@@ -14,8 +27,9 @@ const Sidebar = ({ sidebarOpen, toggleSidebar }) => {
 
       {/* Sidebar navigation */}
       <nav className={`sidebar ${sidebarOpen ? "open" : ""}`}>
-
-        <div id="theme-toggle"><ThemeToggle/></div>
+        <div id="theme-toggle">
+          <ThemeToggle />
+        </div>
 
         <Link to="/categories" title="Home" className="link-icon">
           <FaHome />
@@ -24,7 +38,6 @@ const Sidebar = ({ sidebarOpen, toggleSidebar }) => {
         <Link to="/profile" title="Profile" className="link-icon">
           <FaUser />
         </Link>
-
 
         <Link to="/login" title="Login" className="link-icon">
           <FaSignInAlt />
