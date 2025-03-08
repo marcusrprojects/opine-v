432db6580532bfa6e1e656ebbe683137027c4446
marcusrprojects <marcusr4231@gmail.com>
2025-03-08 15:18:20 -0500

    Formatting improvements.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { AuthProvider } from './context/AuthProvider'; // Adjust the path based on your folder structure
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthProvider"; // Adjust the path based on your folder structure
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <App />
