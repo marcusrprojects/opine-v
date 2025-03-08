686ea70932f8e57b1d4f7c660230fc2178774a91
marcusrprojects <marcusr4231@gmail.com>
2025-03-07 21:54:57 -0500

    Added TagProvider access to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Categories from "./components/Categories";
-import CreateCategory from './components/CreateCategory';
-import CategoryDetail from './components/CategoryDetail.jsx';
-import AddItem from './components/AddItemFlow';
+import CreateCategory from "./components/CreateCategory";
+import CategoryDetail from "./components/CategoryDetail.jsx";
+import AddItem from "./components/AddItemFlow";
 import Profile from "./components/Profile";
 import "./App.css";
-import './styles/variables.css';
+import "./styles/variables.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Sidebar from "./components/Sidebar.jsx"
+import Sidebar from "./components/Sidebar.jsx";
 import "./firebaseConfig";
-import ItemView from './components/ItemView';
-import ReRankFlow from './components/ReRankFlow';
+import ItemView from "./components/ItemView";
+import ReRankFlow from "./components/ReRankFlow";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import EditCategory from './components/EditCategory';
-
+import EditCategory from "./components/EditCategory";
+import { TagProvider } from "./context/TagContext.jsx";
 
 /**
  * App component is the main entry point for the application.
@@ -34,48 +34,59 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div className="App-layout">
-          {/* Pass the sidebar state and toggle function as props */}
-          <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+        <TagProvider>
+          <div className="App-layout">
+            {/* Pass the sidebar state and toggle function as props */}
+            <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
 
-          {/* Main content area */}
-          <div className={`main-content`}>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/categories" element={<Categories />} />
-              <Route 
-                path="/create-category" 
-                element={
-                  <ProtectedRoute>
-                    <CreateCategory />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route path="/categories/:categoryId" element={<CategoryDetail />} />
-              <Route path="/categories/:categoryId/edit" element={<EditCategory />} />
-              <Route 
-                path="/categories/:categoryId/add-item" 
-                element={
-                  <ProtectedRoute>
-                    <AddItem />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route path="/categories/:categoryId/item/:itemId" element={<ItemView />} />
-              <Route 
-                path="/categories/:categoryId/items/:itemId/rerank" 
-                element={
-                  <ProtectedRoute>
-                    <ReRankFlow />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
+            {/* Main content area */}
+            <div className={`main-content`}>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route
+                  path="/create-category"
+                  element={
+                    <ProtectedRoute>
+                      <CreateCategory />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/categories/:categoryId"
+                  element={<CategoryDetail />}
+                />
+                <Route
+                  path="/categories/:categoryId/edit"
+                  element={<EditCategory />}
+                />
+                <Route
+                  path="/categories/:categoryId/add-item"
+                  element={
+                    <ProtectedRoute>
+                      <AddItem />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/categories/:categoryId/item/:itemId"
+                  element={<ItemView />}
+                />
+                <Route
+                  path="/categories/:categoryId/items/:itemId/rerank"
+                  element={
+                    <ProtectedRoute>
+                      <ReRankFlow />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </TagProvider>
       </Router>
     </div>
   );
