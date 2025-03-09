9868988d3e0686eb7147de0be78c667915bd2da7
marcusrprojects <marcusr4231@gmail.com>
2025-03-09 09:35:54 -0400

    Enums for privacy levels (and associated labels).

---
+++ src/constants/privacy.js
@@ -0,0 +1,9 @@
+export const PRIVACY_LEVELS = {
+  PUBLIC: 0,
+  FRIENDS_ONLY: 1,
+};
+
+export const PRIVACY_LABELS = {
+  [PRIVACY_LEVELS.PUBLIC]: "Public",
+  [PRIVACY_LEVELS.FRIENDS_ONLY]: "Friends Only",
+};
