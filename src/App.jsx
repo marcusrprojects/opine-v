74314942a9de03fc4f85a68d4561f8dc91354365
marcusrprojects <marcusr4231@gmail.com>
2025-03-14 09:46:11 -0400

    Removing TagProvider.

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import ForgotPassword from "./components/ForgotPassword";
 
 // Context Providers
 import { LikedCategoriesProvider } from "./context/LikedCategoriesContext";
-import { TagProvider } from "./context/TagContext";
 import { FollowProvider } from "./context/FollowContext";
 
 /**
@@ -37,71 +36,66 @@ function App() {
   return (
     <div className="App">
       <LikedCategoriesProvider>
-        <TagProvider>
-          <FollowProvider>
-            <Router>
-              <div className="App-layout">
-                {/* Sidebar */}
-                <Sidebar
-                  sidebarOpen={sidebarOpen}
-                  toggleSidebar={toggleSidebar}
-                />
+        <FollowProvider>
+          <Router>
+            <div className="App-layout">
+              {/* Sidebar */}
+              <Sidebar
+                sidebarOpen={sidebarOpen}
+                toggleSidebar={toggleSidebar}
+              />
 
-                {/* Main content area */}
-                <div className="main-content">
-                  <Routes>
-                    <Route exact path="/" element={<Feed />} />
-                    <Route path="/categories" element={<Categories />} />
-                    <Route
-                      path="/create-category"
-                      element={
-                        <ProtectedRoute>
-                          <CreateCategory />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path="/categories/:categoryId"
-                      element={<CategoryDetail />}
-                    />
-                    <Route
-                      path="/categories/:categoryId/edit"
-                      element={<EditCategory />}
-                    />
-                    <Route
-                      path="/categories/:categoryId/add-item"
-                      element={
-                        <ProtectedRoute>
-                          <AddItem />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path="/categories/:categoryId/item/:itemId"
-                      element={<ItemView />}
-                    />
-                    <Route
-                      path="/categories/:categoryId/items/:itemId/rerank"
-                      element={
-                        <ProtectedRoute>
-                          <ReRankFlow />
-                        </ProtectedRoute>
-                      }
-                    />
-                    {/* Route for viewing profiles (own or others') */}
-                    <Route path="/profile/:userId?" element={<Profile />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route
-                      path="/forgot-password"
-                      element={<ForgotPassword />}
-                    />
-                  </Routes>
-                </div>
+              {/* Main content area */}
+              <div className="main-content">
+                <Routes>
+                  <Route exact path="/" element={<Feed />} />
+                  <Route path="/categories" element={<Categories />} />
+                  <Route
+                    path="/create-category"
+                    element={
+                      <ProtectedRoute>
+                        <CreateCategory />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/categories/:categoryId"
+                    element={<CategoryDetail />}
+                  />
+                  <Route
+                    path="/categories/:categoryId/edit"
+                    element={<EditCategory />}
+                  />
+                  <Route
+                    path="/categories/:categoryId/add-item"
+                    element={
+                      <ProtectedRoute>
+                        <AddItem />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/categories/:categoryId/item/:itemId"
+                    element={<ItemView />}
+                  />
+                  <Route
+                    path="/categories/:categoryId/items/:itemId/rerank"
+                    element={
+                      <ProtectedRoute>
+                        <ReRankFlow />
+                      </ProtectedRoute>
+                    }
+                  />
+                  {/* Route for viewing profiles (own or others') */}
+                  <Route path="/profile/:userId?" element={<Profile />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                </Routes>
               </div>
-            </Router>
-          </FollowProvider>
-        </TagProvider>
+            </div>
+          </Router>
+        </FollowProvider>
       </LikedCategoriesProvider>
     </div>
   );
