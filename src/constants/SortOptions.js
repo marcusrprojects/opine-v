eb809310704438f9a205f549480549c2345d326c
marcusrprojects <marcusr4231@gmail.com>
2025-03-21 19:34:04 -0400

    sorting enums implementation.

---
+++ src/constants/SortOptions.js
@@ -0,0 +1,10 @@
+// constants/SortOptions.js
+const SortOptions = {
+  UPDATED_DESC: "updated_desc",
+  UPDATED_ASC: "updated_asc",
+  ALPHA_ASC: "alpha_asc",
+  ALPHA_DESC: "alpha_desc",
+  MOST_LIKED: "most_liked",
+};
+
+export default SortOptions;
