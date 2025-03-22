f756b50e57ddfc48007c3e96cce2ce21fb1b4707
marcusrprojects <marcusr4231@gmail.com>
2025-03-21 20:13:22 -0400

    Now, privacy constants are made into privacy enums.

---
+++ src/enums/PrivacyEnums.js
@@ -0,0 +1,12 @@
+const UserPrivacy = Object.freeze({
+  PUBLIC: "public",
+  PRIVATE: "private",
+});
+
+const CategoryPrivacy = Object.freeze({
+  EVERYONE: "everyone",
+  ONLY_ME: "only_me",
+  FOLLOWERS_ONLY: "followers_only",
+});
+
+export { UserPrivacy, CategoryPrivacy };
