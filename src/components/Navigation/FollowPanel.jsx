aeef4236bb546090e9c8ff67e8e5f4eb137da91f
marcusrprojects <marcusr4231@gmail.com>
2025-03-22 21:51:29 -0400

    Considers whether the follow request is pending.

--- src/components/Navigation/FollowPanel.jsx
+++ src/components/Navigation/FollowPanel.jsx
@@ -1,26 +1,36 @@
 import PropTypes from "prop-types";
 import Panel from "./Panel";
 import Button from "./Button";
-import { FaUserPlus, FaUserMinus } from "react-icons/fa";
+import { FaUserPlus, FaUserMinus, FaHourglassHalf } from "react-icons/fa";
+import { FollowStatus } from "../../enums/ModeEnums";
 
 /**
  * FollowPanel allows users to follow/unfollow another user.
- * - Uses consistent styling with other panels.
+ * Handles: follow, unfollow, and pending request state.
  */
-const FollowPanel = ({ isFollowing, onToggleFollow }) => {
+const FollowPanel = ({ followStatus, onToggleFollow }) => {
+  const isFollowing = followStatus === FollowStatus.FOLLOWING;
+  const isPending = followStatus === FollowStatus.PENDING;
+
   return (
     <Panel>
-      <Button
-        onClick={onToggleFollow}
-        title={isFollowing ? "Unfollow" : "Follow"}
-        icon={isFollowing ? FaUserMinus : FaUserPlus}
-      />
+      {isFollowing ? (
+        <Button onClick={onToggleFollow} title="Unfollow" icon={FaUserMinus} />
+      ) : isPending ? (
+        <Button
+          onClick={onToggleFollow}
+          title="Cancel Request"
+          icon={FaHourglassHalf}
+        />
+      ) : (
+        <Button onClick={onToggleFollow} title="Follow" icon={FaUserPlus} />
+      )}
     </Panel>
   );
 };
 
 FollowPanel.propTypes = {
-  isFollowing: PropTypes.bool.isRequired,
+  followStatus: PropTypes.oneOf(Object.values(FollowStatus)).isRequired,
   onToggleFollow: PropTypes.func.isRequired,
 };
 
