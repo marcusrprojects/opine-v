815c402ecb434357a956702d3f52d79cce3831bd
marcusrprojects <marcusr4231@gmail.com>
2025-03-23 01:37:24 -0400

    Better support for follow requests.

--- src/components/Navigation/EditLogoutPanel.jsx
+++ src/components/Navigation/EditLogoutPanel.jsx
@@ -1,28 +1,51 @@
 import PropTypes from "prop-types";
 import Panel from "./Panel";
 import Button from "./Button";
-import { FaEdit, FaSignOutAlt } from "react-icons/fa";
+import { FaEdit, FaSignOutAlt, FaUserClock } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/useAuth";
+import "../../styles/EditLogoutPanel.css";
 
-const EditLogoutPanel = ({ onEdit }) => {
+const EditLogoutPanel = ({
+  onEdit,
+  onToggleFollowRequests,
+  showFollowRequests,
+  hasFollowRequests,
+}) => {
   const navigate = useNavigate();
   const { logout } = useAuth();
 
   return (
-    <Panel>
-      <Button onClick={onEdit} title="Edit Profile" icon={FaEdit} />
-      <Button
-        onClick={() => logout(navigate)}
-        title="Logout"
-        icon={FaSignOutAlt}
-      />
-    </Panel>
+    <div className="edit-logout-wrapper">
+      <Panel>
+        <Button onClick={onEdit} title="Edit Profile" icon={FaEdit} />
+        {hasFollowRequests && (
+          <Button
+            onClick={onToggleFollowRequests}
+            title="Follow Requests"
+            icon={FaUserClock}
+          />
+        )}
+        <Button
+          onClick={() => logout(navigate)}
+          title="Logout"
+          icon={FaSignOutAlt}
+        />
+      </Panel>
+      {showFollowRequests && (
+        <div className="follow-requests-overlay">
+          {/* FollowRequests component will be rendered from parent */}
+        </div>
+      )}
+    </div>
   );
 };
 
 EditLogoutPanel.propTypes = {
   onEdit: PropTypes.func.isRequired,
+  onToggleFollowRequests: PropTypes.func.isRequired,
+  showFollowRequests: PropTypes.bool.isRequired,
+  hasFollowRequests: PropTypes.bool.isRequired,
 };
 
 export default EditLogoutPanel;
