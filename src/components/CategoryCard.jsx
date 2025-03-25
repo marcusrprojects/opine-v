2ea6410b6ea629bba64a4acf96dda7f5a881c653
marcusrprojects <marcusr4231@gmail.com>
2025-03-25 12:23:49 -0400

    Field not sufficiently important to be included in category card.
    Added onHover functionality to switch between showing the username and the tags.

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,18 +2,27 @@ import PropTypes from "prop-types";
 import Card from "./Card";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import "../styles/CategoryCard.css";
+import { useState } from "react";
+import { useUserCache } from "../context/useUserCache";
 
 const CategoryCard = ({ category, onClick, onLike, liked }) => {
-  const fieldNames =
-    category.fields?.length > 0
-      ? category.fields.map((f) => f.name).join(", ")
-      : "No fields";
+  const [isHovered, setIsHovered] = useState(false);
 
+  // Use our custom context/hook to get the username for the creator ID.
+  const { getUsername } = useUserCache();
+  const creatorUsername = getUsername(category.createdBy) || "Unknown";
+
+  // Show tags normally.
   const tagText =
     category.tags?.length > 0 ? category.tags.join(", ") : "No tags";
 
   return (
-    <Card onClick={onClick}>
+    <Card
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      // onMouseEnter={() => console.log("hey")}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="card-header">
         <h4 className="card-title">{category.name}</h4>
         <span
@@ -26,8 +35,12 @@ const CategoryCard = ({ category, onClick, onLike, liked }) => {
           {liked ? <FaHeart /> : <FaRegHeart />}
         </span>
       </div>
-      <p className="card-content category-card-content">{fieldNames}</p>
-      <span className="card-tags">{tagText}</span>
+      <div className="card-tags-container">
+        <span className={`card-tags ${isHovered ? "show-username" : ""}`}>
+          <span className="tags-text">{tagText}</span>
+          <span className="username-text">@{creatorUsername}</span>
+        </span>
+      </div>
     </Card>
   );
 };
@@ -36,6 +49,7 @@ CategoryCard.propTypes = {
   category: PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    createdBy: PropTypes.string.isRequired,
     fields: PropTypes.arrayOf(
       PropTypes.shape({
         name: PropTypes.string.isRequired,
@@ -47,4 +61,5 @@ CategoryCard.propTypes = {
   onLike: PropTypes.func.isRequired,
   liked: PropTypes.bool.isRequired,
 };
+
 export default CategoryCard;
